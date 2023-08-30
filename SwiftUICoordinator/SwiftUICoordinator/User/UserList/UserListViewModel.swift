9341c99aef26c3965702b60dae35739a0c72779d
Gagan Vishal Mishra <gagan.vishal@mmltd.co.za>
2023-08-30 22:19:02 +0200

    Added UserListViewModel

--- SwiftUICoordinator/SwiftUICoordinator/User/UserList/UserListViewModel.swift
+++ SwiftUICoordinator/SwiftUICoordinator/User/UserList/UserListViewModel.swift
@@ -5,4 +5,19 @@
 //  Created by Gagan Vishal  on 2023/08/30.
 //
 
-import Foundation
+import SwiftUI
+
+class UserListViewModel: ObservableObject {
+    
+    @Published var users: [User] = []
+    
+    // MARK: - init
+    init() {}
+    
+    func fetchUsers() {
+        // Add 1 second delay for dummy server request
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            self?.users = User.dummyUser
+        }
+    }
+}
